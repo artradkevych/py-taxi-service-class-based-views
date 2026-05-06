9ef8b0af1771f367a54e9d25336af396c86c6b40
Artem <artemradckevich@gmail.com>
2026-05-06 10:46:25 +0300

    'Fixed one-line imports'

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from .views import (index, ManufacturerListView,
-                    CarListView, DriverListView,
-                    DriverDetailView, CarDetailView)
+from .views import (
+    index,
+    ManufacturerListView,
+    CarListView,
+    DriverListView,
+    DriverDetailView,
+    CarDetailView
+)
 
 urlpatterns = [
     path("", index, name="index"),
